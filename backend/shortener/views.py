485520e9ef53cb65cabdf5f1efc3c41580b3ca00
kali <kuriakosant2003@gmail.com>
2024-08-12 12:00:00 +0300

    write initial views

--- backend/shortener/views.py
+++ backend/shortener/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from .models import URL
+import random
+import string
 
-# Create your views here.
+# Helper function to generate a short URL
+def generate_short_url():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+
+# View to handle URL shortening
+def shorten_url(request):
+    if request.method == "POST":
+        original_url = request.POST.get('original_url')
+        short_url = generate_short_url()
+
+        url = URL.objects.create(original_url=original_url, short_url=short_url)
+        return render(request, 'shortener/result.html', {'short_url': short_url})
+    return render(request, 'shortener/home.html')
+
+# View to handle redirecting to the original URL
+def redirect_url(request, short_url):
+    url = get_object_or_404(URL, short_url=short_url)
+    return HttpResponseRedirect(url.original_url)
