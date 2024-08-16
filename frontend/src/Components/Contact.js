19424702d9909575bbff490ca719ec789dc414fa
kali <kuriakosant2003@gmail.com>
2024-08-16 12:00:00 +0300

    update contact component

--- frontend/src/Components/Contact.js
+++ frontend/src/Components/Contact.js
@@ -1,19 +1,21 @@
 // src/Components/Contact.js
 import React from 'react';
-import { FaGithub } from 'react-icons/fa'; // Importing Github icon from react-icons
+import { FaGithub } from 'react-icons/fa';
 import '../Styles/Contact.css';
 
 const Contact = () => {
-    return (
-        <div className="contact-container">
-            <h1>Contact Me</h1>
-            <p>Name: Kyriakos Antoniadis</p>
-            <p>Email: kuriakosant2003@gmail.com</p>
-            <a href="https://github.com/kuriakosant" target="_blank" rel="noopener noreferrer">
-                <FaGithub size={40} /> {/* Github icon */}
-            </a>
-        </div>
-    );
+  return (
+    <div className="contact-container">
+      <h1>Contact Me</h1>
+      <p>Name: Kyriakos Antoniadis</p>
+      <p>Email: kuriakosant2003@gmail.com</p>
+      <div className="social-links">
+        <a href="https://github.com/kuriakosant" target="_blank" rel="noreferrer">
+          <FaGithub size={40} className="icon" />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
