775c6f5cb9cce7ff1b2b28610ee3a5612a3054f1
kali <kuriakosant2003@gmail.com>
2024-08-16 12:00:00 +0300

    update header

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,21 +1,29 @@
 // src/Components/Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../Styles/Header.css'; // Updated path
+import '../Styles/Header.css';
 
 const Header = () => {
-    return (
-        <header>
-            <h1>URL Shortener</h1>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">URL Shortener</Link>
+      </div>
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
