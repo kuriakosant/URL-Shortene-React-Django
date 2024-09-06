2b7fd9e40264d190ce8c0c61e93d4d9fa8006fe5
kali <kuriakosant2003@gmail.com>
2024-09-06 12:00:00 +0300

    update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './Components/Header';
 import Home from './Components/Home';
 import About from './Components/About';
 import Contact from './Components/Contact';
-import Header from './Components/Header';
+import Footer from './Components/Footer';
+import './Styles/App.css';
 
-function App() {
-    return (
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-            </Routes>
-        </Router>
-    );
-}
+const App = () => {
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer /> 
+      </div>
+    </Router>
+  );
+};
 
 export default App;
